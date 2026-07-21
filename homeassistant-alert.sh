b9fab00e286cbabd91f9c7900b9f4584a3a30605
LinuxforPunks <121705807+LinuxforPunks@users.noreply.github.com>
2026-07-21 21:05:02 +0100

    Update MQTT credentials in homeassistant-alert.sh

--- homeassistant-alert.sh
+++ homeassistant-alert.sh
@@ -4,7 +4,7 @@ export DISPLAY=:0
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
 
-mosquitto_sub -h 192.168.1.170 -u "vicky" -P "thalia" -t "desktop/gaming/alerts" | while read -r payload; do
+mosquitto_sub -h 192.168.1.170 -u "USERNAME" -P "PASSWORD" -t "desktop/gaming/alerts" | while read -r payload; do
     # Trigger pop-up notification
     notify-send "Home Assistant" "$payload"
     # Trigger audio alert
